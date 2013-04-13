f9ccc8383af6a6ab10f3c2a254b303924c9028c5
Natasha A. Hull <musicalnat@gmail.com>
2013-04-13 10:29:15 -0700

    Created new website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slartibartfast::Application.config.secret_token = '2e0b94eecd395ccfd19bba11c4c772757aac76bfc11e61cda1572290de72a08dac6b9a4d97d0578ff460dad35cbbafc289ba1fce7c8dfb3faf518997c420e15e'
